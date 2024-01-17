7fd98a938b91e90fc426b993fb4a998a50cd66c1
Onyedikachi Nwanguma <119358391+HexTech19@users.noreply.github.com>
2024-01-17 22:04:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const input = document.querySelector('.text-input');
 const addButton = document.querySelector('.add-btn');
-const list = document.querySelector('.list-items')
+const list = document.querySelector('.list-items');
+const deleteButton = document.querySelectorAll('.del-btn');
 
+
+//Getting the input value
 let inputVal;
 input.addEventListener('keyup', () => {
   setTimeout(() => {
      inputVal = input.value;
-  },1000)
+  },500)
 })
 
-let listItem;
+//adding the todo to the list
+
 addButton.addEventListener('click', () => {
   let newLi = document.createElement('li');
-  newLi.innerHTML = `<li>${inputVal}</li> <button><i class="fa-solid fa-trash"></i></button></i>`
-  // newLi.innerText = inputVal;
+  newLi.innerHTML = `${inputVal}<button class="del-btn"><i class="fa-solid fa-trash"></i></button></i>`
   list.appendChild(newLi);
+});
+
+//deleting the todo
+document.body.addEventListener('click', (e) => {
+  if (e.target.matches('.del-btn')) {
+    const li = e.target.parentElement;
+    li.remove();
+  }
 })
