4714198353e8f667b5d62bb8e38bb88aa7a8ed0c
Onyedikachi Nwanguma <119358391+HexTech19@users.noreply.github.com>
2024-01-18 23:56:15 +0100

    update

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
 const input = document.querySelector('.text-input');
 const addButton = document.querySelector('.add-btn');
-const list = document.querySelector('.list-items');
+const list = document.querySelector('.list-item');
+const todoDiv = document.querySelector('.todo-div');
 const deleteButton = document.querySelectorAll('.del-btn');
-const toDo = document.querySelector('.to-do');
+
 
 //Getting the input value
+let inputVal;
 input.addEventListener('keyup', () => {
   setTimeout(() => {
      inputVal = input.value;
+     console.log(inputVal)
   },500)
 });
 
-//adding the todo to the list
-function addTodo(e) {
-  e.preventDefault();
-  const newLi = document.createElement('li');
-  newLi.innerText = `${inputVal}`
-  newLi.classList.add('todo-li');
-  toDo.appendChild(newLi);
+// //adding the todo to the list
+// function addTodo(e) {
+//   let newLi = document.createElement('li');
+//   newLi.innerText = `${inputVal}`
+//   newLi.classList.add('todo-li');
+//   toDo.appendChild(newLi);
 
-  //checked button
-  // const doneBtn = document.createElement('input');
-  // doneBtn.type="checkbox";
-  // doneBtn.classList.add('check');
-  // toDo.appendChild(doneBtn);
+//   //checked button
+//   // const doneBtn = document.createElement('input');
+//   // doneBtn.type="checkbox";
+//   // doneBtn.classList.add('check');
+//   // toDo.appendChild(doneBtn);
 
-  //delete button
-  const btn = document.createElement('button');
-  btn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
-  btn.classList.add('del-btn');
-  toDo.appendChild(btn);
-}
+//   //delete button
+//   const btn = document.createElement('button');
+//   btn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+//   btn.classList.add('del-btn');
+//   toDo.appendChild(btn);
+// }
 
-//making the add button add the todo
-addButton.addEventListener('click', addTodo);
+// //making the add button add the todo
+// addButton.addEventListener('click', addTodo);
 
-//deleting the todo
-document.body.addEventListener('click', (e) => {
-  if (e.target.matches('.del-btn')) {
-    const li = e.target.parentElement;
-    li.remove();
-  }
-});
+// //deleting the todo
+// document.body.addEventListener('click', (e) => {
+//   if (e.target.matches('.del-btn')) {
+//     const li = e.target.parentElement;
+//     li.remove();
+//   }
+// });
